2a967e7064ab66a3205ee6d5a9e9278ae3319db5
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-30 01:12:43 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // makes deployment easier
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
