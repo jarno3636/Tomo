ab8c14ae50d41314f518ba0fc92d9df7dd2e8b0b
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-30 01:27:23 -0500

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,20 @@
+// lib/constants.ts
+
+export const CONTRACT_ADDRESS = '0xacc4b4d66bc7dcab0ae15ee0effece546ceb68d2'
+
+export const MINT_PRICE_ETH = '0.001' // in ETH
+
+export const SITE_URL = 'https://tomagotchu.xyz'
+
+export const FRAME_BUTTONS = [
+  {
+    label: 'Mint Yours',
+    action: 'link',
+    target: `${SITE_URL}/mint`,
+  },
+  {
+    label: 'View Collection',
+    action: 'link',
+    target: `${SITE_URL}/myo`,
+  },
+]
