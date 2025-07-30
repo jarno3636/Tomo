18e8f81d09c9da308dfee6f1ff8e00cafa44764f
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-29 23:59:31 -0500

    Create Confetti.tsx

---
+++ components/Confetti.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect } from 'react'
+import confetti from 'canvas-confetti'
+
+export default function Confetti() {
+  useEffect(() => {
+    confetti({
+      particleCount: 200,
+      spread: 90,
+      origin: { y: 0.6 }
+    })
+  }, [])
+
+  return null
+}
