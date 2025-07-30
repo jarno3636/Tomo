98eda0a12aaad39774ec5f646d6c9916bffa7622
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-30 18:51:38 -0500

    Update TomagotchuABI.ts

--- lib/TomagotchuABI.ts
+++ lib/TomagotchuABI.ts
@@ -1,6 +1,13 @@
-export const CONTRACT_ADDRESS = "0xacc4b4d66bc7dcab0ae15ee0effece546ceb68d2"; // Replace if needed
+export const CONTRACT_ADDRESS = "0xacc4b4d66bc7dcab0ae15ee0effece546ceb68d2";
 
 export const TomagotchuABI = [
+  {
+    inputs: [],
+    name: "mint",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
   {
     inputs: [
       { internalType: "uint256", name: "tokenId", type: "uint256" },
@@ -21,5 +28,24 @@ export const TomagotchuABI = [
     outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
     type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalMinted",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "owner", type: "address" },
+      { indexed: true, internalType: "uint256", name: "tokenId", type: "uint256" },
+      { indexed: false, internalType: "uint8", name: "color", type: "uint8" },
+      { indexed: false, internalType: "uint8", name: "shape", type: "uint8" },
+      { indexed: false, internalType: "uint8", name: "animal", type: "uint8" },
+    ],
+    name: "TomagotchuMinted",
+    type: "event",
   },
 ];
