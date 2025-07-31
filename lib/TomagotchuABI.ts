1476ead724233da71f96aba67f6c7dff34d58610
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-07-31 06:58:44 -0500

    Update TomagotchuABI.ts

--- lib/TomagotchuABI.ts
+++ lib/TomagotchuABI.ts
@@ -4,24 +4,25 @@ const TomagotchuABI = [
   {
     inputs: [
       { internalType: "uint256", name: "tokenId", type: "uint256" },
-      { internalType: "address", name: "user", type: "address" }
+      { internalType: "address", name: "user", type: "address" },
     ],
     name: "generateTraits",
     outputs: [
       { internalType: "uint8", name: "", type: "uint8" },
       { internalType: "uint8", name: "", type: "uint8" },
-      { internalType: "uint8", name: "", type: "uint8" }
+      { internalType: "uint8", name: "", type: "uint8" },
     ],
     stateMutability: "pure",
-    type: "function"
+    type: "function",
   },
   {
     inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
     name: "ownerOf",
     outputs: [{ internalType: "address", name: "", type: "address" }],
     stateMutability: "view",
-    type: "function"
-  }
-]
+    type: "function",
+  },
+] as const
 
 export default TomagotchuABI
+export { TomagotchuABI }
